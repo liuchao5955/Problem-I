df121f8215d47fb77ecf392420634b669dacebd9
liu chao <liuchao5955@163.com>
2012-08-10 01:57:48 +0800

    first commit

---
+++ Problem1.py
@@ -0,0 +1,15 @@
+'''
+Created on 2012-8-10
+
+@author: Administrator
+'''
+
+a = 'ball'
+b = ['at', '', '', '', 'ball', '', '', 'car', '', 'dad', '', '']
+def method():
+    for x in range(len(b)):
+        if b[x] == a:
+            return x
+    return -1    
+
+print method()
